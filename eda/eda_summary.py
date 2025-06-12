78fd19157956affcf5062ee4954a95760035f902
Dagiayy <ayenewdagmawi@gmail.com>
2025-06-12 08:20:37 -0700

    initialize

---
+++ eda/eda_summary.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('data/insurance_data.csv')
+
+print("Shape of the dataset:", df.shape)
+print("Columns:", df.columns.tolist())
+print("First 5 rows:\n", df.head())
